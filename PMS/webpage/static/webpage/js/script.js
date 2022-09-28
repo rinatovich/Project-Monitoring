58a3b841392300d7cab08ab598eb7df0b3a4632b
rinatovic <mvrinatovicwo@gmail.com>
2022-09-28 18:32:20 +0500

    frontend filters

--- PMS/webpage/static/webpage/js/script.js
+++ PMS/webpage/static/webpage/js/script.js
@@ -9,3 +9,20 @@ for(let s=0; s<statusFields.length; s++){
         
     });
 }
+
+
+function urlRedirecter(elements,param_name){
+    for(let dd =0; dd< elements.length; dd++){
+        elements[dd].addEventListener('click',(event)=>{
+            event.preventDefault();
+            id = event.target.id;
+            let url = new URL(`${window.location.href}`);
+            url.searchParams.set(param_name, id);
+            window.location.replace(url);
+        })
+    }
+}
+
+let dropdown_item_managers = document.querySelectorAll('.managers_filter_item');
+urlRedirecter(dropdown_item_managers, 'manager');
+
--- PMS/webpage/views.py
+++ PMS/webpage/views.py
@@ -28,17 +28,30 @@ class ProjectCategory(DataMixin, ListView):
     model = Project
     template_name = 'webpage/index.html'
     context_object_name = 'projects'
+    active_manager = None
+    active_company = None
 
     def get_queryset(self):
         if(self.request.GET):
-            managers = self.request.GET['manager'].split(',')
-            return Project.objects.filter(cat__slug=self.kwargs['cat_slug'], manager__pk__in=managers).select_related('cat')
+            if (self.request.GET['manager']):
+                managers = self.request.GET['manager'].split(',')
+                self.active_manager = Manager.objects.filter(id__in=managers)
+            if (self.request.GET['company']):
+                companies = self.request.GET['company'].split(',')
+                self.active_company = Company.objects.filter(id__in=companies)
+            return Project.objects.filter(cat__slug=self.kwargs['cat_slug'], manager__pk__in=managers, company__pk__in=companies).select_related('cat')
         else:
             return Project.objects.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        if self.active_manager:
+            context['active_manager'] = self.active_manager
+        if self.active_company:
+            context['active_company'] = self.active_company
         context['iter'] = 1
+        context['managers'] = Manager.objects.all()
+        context['companies'] = Company.objects.all()
         c = Category.objects.get(slug=self.kwargs['cat_slug'])
         context['category'] = c
         c_def = self.get_user_context(title='Category - ' + str(c.title),
